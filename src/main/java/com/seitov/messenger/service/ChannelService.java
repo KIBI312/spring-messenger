27cd6c6fdb0e0407e3ee2f0a62277ddee7ab6532
KIBI312 <isken00.kg@gmail.com>
2022-11-16 01:59:06 +0600

    Adds Channel related services

---
+++ src/main/java/com/seitov/messenger/service/ChannelService.java
@@ -0,0 +1,70 @@
+package com.seitov.messenger.service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.seitov.messenger.entity.Article;
+import com.seitov.messenger.entity.Channel;
+import com.seitov.messenger.entity.Channel.AccessType;
+import com.seitov.messenger.exception.IllegalDataFormatException;
+import com.seitov.messenger.exception.ResourceNotFoundException;
+import com.seitov.messenger.repository.ArticleRepository;
+import com.seitov.messenger.repository.ChannelRepository;
+
+@Service
+public class ChannelService {
+
+    private ChannelRepository channelRepository;
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    public ChannelService(ChannelRepository channelRepository, ArticleRepository articleRepository) {
+        this.channelRepository = channelRepository;
+        this.articleRepository = articleRepository;
+    }
+
+    public Channel create(Channel toCreate) throws IllegalDataFormatException {
+        try {
+            return channelRepository.save(toCreate);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalDataFormatException("Name and accessType cannot be null!");
+        }
+    }
+
+    public Channel get(UUID id) throws ResourceNotFoundException {
+        Optional<Channel> channel = channelRepository.findById(id);
+        if(channel.isEmpty()) {
+            throw new ResourceNotFoundException("Channel with such id doesnt exist");
+        }
+        return channel.get();
+    }
+
+    public Channel update(Channel channel) throws IllegalDataFormatException {
+        try {
+            return channelRepository.save(channel);   
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalDataFormatException("Error occured while attempting to update this channel!", e.getCause());
+        }
+    }
+
+    public void delete(UUID id) {
+        channelRepository.deleteById(id);
+    }
+
+    public List<Channel> getOpenChannels(Pageable pageable) {
+        List<Channel> openChannels = channelRepository.findAllByAccessType(AccessType.open, pageable);
+        return Collections.unmodifiableList(openChannels);
+    }
+
+    public List<Article> getChannelArticles(Channel channel, Pageable pageable) {
+        return articleRepository.findAllByChannelOrderByTimestampDesc(channel, pageable);
+    }
+
+}
