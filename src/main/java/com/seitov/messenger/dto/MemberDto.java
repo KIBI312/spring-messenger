4a818e665abb848f0ccb21d03d8d42f068514676
KIBI312 <isken00.kg@gmail.com>
2022-11-17 02:48:46 +0600

    Adds DTO representing member on channel

---
+++ src/main/java/com/seitov/messenger/dto/MemberDto.java
@@ -0,0 +1,17 @@
+package com.seitov.messenger.dto;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDto {
+    
+    private UUID channelId;
+    private String username;
+
+}
